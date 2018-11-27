c41e7e5cccceaec1d89ac5451ae08723f2d949f6
William Scott <wis15@skrs-g36-006.ese.ic.ac.uk>
2018-11-27 19:42:30 +0000

    import firedrake.conditional and firedrake.Constant

--- thwaites/meltrate_param.py
+++ thwaites/meltrate_param.py
@@ -1,5 +1,5 @@
-import firedrake
-
+from firedrake import conditional
+from firedrake import Constant
 
 
 class MeltRateParam():
@@ -100,7 +100,7 @@ class MeltRateParam():
                                  0.0)  # units of Km/s , add in meltrate to capture flux of water through boundary Jenkins et al 2001 eq 25
         Q_s_bc = conditional(self.z > 0 - self.dz_calc, -(wb + self.gammaS) * (loc_Sb - self.S), 0.0)
 
-        return Q_ice, Q_mixed_bc, Q_mixed, Q_latent, Q_s_bc, wb, Tb, self.Pfull  # these are all still expressions
+        return Q_ice, Q_mixed_bc, Q_mixed, Q_latent, Q_s_bc, wb, loc_Tb, self.Pfull  # these are all still expressions
 
 
     def three_eq_param_meltrate(self):
@@ -152,4 +152,4 @@ class MeltRateParam():
                                  0.0)  # units of Km/s , add in meltrate to capture flux of water through boundary Jenkins et al 2001 eq 25
         Q_s_bc = conditional(self.z > 0 - self.dz_calc, -(wb + self.gammaS) * (loc_Sb - self.S), 0.0)
 
-        return Q_ice, Q_mixed_bc, Q_mixed, Q_latent, Q_s_bc, wb, Tb, self.Pfull  # these are all still expressions
+        return Q_ice, Q_mixed_bc, Q_mixed, Q_latent, Q_s_bc, wb, loc_Tb, self.Pfull  # these are all still expressions
