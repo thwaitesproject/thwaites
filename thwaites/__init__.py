ec635947e39302f9aa1e4a3ae7b7ec0959bba56c
William Scott <wis15@skrs-g36-006.ese.ic.ac.uk>
2018-11-27 13:38:42 +0000

    add module for melt rate parameterisation

--- thwaites/__init__.py
+++ thwaites/__init__.py
@@ -5,6 +5,7 @@ from .time_stepper import ERKEuler, ERKLSPUM2, ERKLPUM2, ERKMidpoint, SSPRK33, \
     BackwardEuler, DIRK22, DIRK23, DIRK33, DIRK43, DIRKLSPUM2, DIRKLPUM2
 from .coupled_integrators import CrankNicolsonSaddlePointTimeIntegrator, PressureProjectionTimeIntegrator
 from .assembledschur import AssembledSchurPC
+from .meltrate_param import MeltRateParam
 
 from ._version import get_versions
 __version__ = get_versions()['version']
