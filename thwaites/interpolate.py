78d6d2e3cc7f36498a52d022c912f62358a0bfe6
Will Scott <wis15@ic.ac.uk>
2021-05-18 11:56:38 +0100

    Add Daniel Shapero's interpolate code for netcdf / geotiff from icepack

---
+++ thwaites/interpolate.py
@@ -0,0 +1,110 @@
+#This code coems from Icepack c thanks to Daniel Shapero
+# It reads in netcdf / geotiff files and overoloads 
+# firedrake interpolate so that it can access this data...
+r"""Functions for interpolating gridded remote sensing data sets to finite
+element spaces"""
+
+import numpy as np
+import ufl
+import firedrake
+import rasterio
+from scipy.interpolate import RegularGridInterpolator
+
+
+def _sample(dataset, X, method):
+    xres = dataset.res[0]
+    bounds = dataset.bounds
+    print("bounds", bounds)
+    print(type(bounds))
+    print("dataset", dataset)
+    xmin = max(X[:, 0].min() - 2 * xres, bounds.left)
+    print("X[:, 0].min() - 2 * xres",X[:, 0].min() - 2 * xres)
+    print("or")
+    print("bounds.left", bounds.left)
+    xmax = min(X[:, 0].max() + 2 * xres, bounds.right)
+
+
+    print("X[:, 0].max() + 2 * xres", X[:, 0].max() + 2 * xres)
+    print("or")
+    print("bounds.right", bounds.right)
+
+    ymin = max(X[:, 1].min() - 2 * xres, bounds.bottom)
+    print("X[:, 1].min() - 2 * xres", X[:, 1].min() - 2 * xres)
+    print("or")
+    print("bounds.bottom", bounds.bottom)
+
+    ymax = min(X[:, 1].max() + 2 * xres, bounds.top)
+    print("X[:, 1].max() + 2 * xres", X[:, 1].max() + 2 * xres)
+    print("or")
+    print("bounds.bottom", bounds.top)
+
+    print("xres", xres)
+    print("xmin", xmin)
+    print("xmax", xmax)
+    print("ymin", ymin)
+    print("ymax", ymax)
+    
+
+    window = rasterio.windows.from_bounds(
+        left=xmin,
+        right=xmax,
+        bottom=ymin,
+        top=ymax,
+        width=dataset.width,
+        height=dataset.height,
+        transform=dataset.transform
+    )
+    window = window.round_lengths(op='ceil').round_offsets(op='floor')
+    transform = rasterio.windows.transform(window, dataset.transform)
+
+    upper_left = transform * (0, 0)
+    lower_right = transform * (window.width - 1, window.height - 1)
+    xs = np.linspace(upper_left[0], lower_right[0], window.width) 
+    ys = np.linspace(lower_right[1], upper_left[1], window.height)
+
+    data = np.flipud(dataset.read(indexes=1, window=window, masked=True)).T
+    interpolator = RegularGridInterpolator((xs, ys), data, method=method, bounds_error=False, fill_value=None)
+    return interpolator(X, method=method)
+
+
+def interpolate(f, Q, method='linear'):
+    r"""Interpolate an expression or a gridded data set to a function space
+
+    Parameters
+    ----------
+    f : rasterio dataset or tuple of rasterio datasets
+        The gridded data set for scalar fields or the tuple of gridded data
+        sets for each component
+    Q : firedrake.FunctionSpace
+        The function space where the result will live
+
+    Returns
+    -------
+    firedrake.Function
+        A finite element function defined on `Q` with the same nodal values
+        as the data `f`
+    """
+    if isinstance(f, (ufl.core.expr.Expr, firedrake.Function)):
+        return firedrake.interpolate(f, Q)
+
+    mesh = Q.mesh()
+    element = Q.ufl_element()
+    if len(element.sub_elements()) > 0:
+        element = element.sub_elements()[0]
+
+    V = firedrake.VectorFunctionSpace(mesh, element)
+    X = firedrake.interpolate(mesh.coordinates, V).dat.data_ro
+
+    q = firedrake.Function(Q)
+
+    if isinstance(f, rasterio.DatasetReader):
+        q.dat.data[:] = _sample(f, X, method)
+    elif (isinstance(f, tuple) and
+          all(isinstance(fi, rasterio.DatasetReader) for fi in f)):
+        for i, fi in enumerate(f):
+            q.dat.data[:, i] = _sample(fi, X, method)
+    else:
+        raise ValueError('Argument must be a rasterio data set or a tuple of '
+                         'data sets!')
+
+    return q
