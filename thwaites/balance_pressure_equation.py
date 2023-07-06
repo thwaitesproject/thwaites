65c0f90da13d38bccb5b9ad6b6ce765c8543a825
Stephan Kramer <s.kramer@imperial.ac.uk>
2023-07-06 11:40:32 +0100

    Fix lint

--- thwaites/balance_pressure_equation.py
+++ thwaites/balance_pressure_equation.py
@@ -21,11 +21,11 @@ class BalancePressurePoisson(BaseTerm):
         p_b = trial
 
         F = dot(grad(xi), grad(p_b)) * self.dx
-        
+
         for id, bc in bcs.items():
             # for vertical side walls with melting and slope add a pressure gradient
-            F -= xi * bc['gradpb'] * self.ds(id) 
-        
+            F -= xi * bc['gradpb'] * self.ds(id)
+
         return F
 
 
