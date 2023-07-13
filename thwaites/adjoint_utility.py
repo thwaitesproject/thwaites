3bede45fddadae387ef9fd8ae0c0c63d9bbf8547
Will Scott <wis15@ic.ac.uk>
2023-07-13 14:22:41 +0100

    Adjoint setups with L2 boundary norms
    ice_shelf_coarse.py now has GammaT as a control - this required changing
    the melt parameterisation to the Jenkins2010/ISOMIP formulation.
    
    ice_shelf_coarse_open_balp.py is a new file with the depth profile
    objective function for sensitivies only.
    
    I initially tried with the new balance pressure code solving for
    the pressure that balances density/geostrophy but this was causing
    undulations at the rhs. Maybe this is because the sloped domain
    has isopycnals that are not flat at the boundary but the balance pressure
    solve was enforcing zero at the boundary... not sure..

--- thwaites/adjoint_utility.py
+++ thwaites/adjoint_utility.py
@@ -4,18 +4,19 @@ import numpy
 
 
 class DiagnosticBlock(Block):
-    def __init__(self, f, function):
+    def __init__(self, f, function, riesz_options={'riesz_representation': 'L2'}):
         super().__init__()
         self.add_dependency(function)
         self.add_output(function.block_variable)
         self.f = f
         self.f_name = function.name()
+        self.riesz_options = riesz_options
 
     def recompute_component(self, inputs, block_variable, idx, prepared):
         return block_variable.checkpoint
 
     def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
-        out = inputs[0]._ad_convert_type(adj_inputs[0], options={'riesz_representation': 'L2'})
+        out = inputs[0]._ad_convert_type(adj_inputs[0], options=self.riesz_options)
         out.rename('adjoint_'+self.f_name)
         self.f.write(out)
         return 0
