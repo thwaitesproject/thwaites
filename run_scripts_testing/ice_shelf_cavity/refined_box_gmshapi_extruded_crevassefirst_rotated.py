c3c70ef028abf56fc82de3c71c900d54b6dd78a8
Will Scott <wis15@ic.ac.uk>
2023-09-19 04:53:01 +0100

    Add two gmsh api scripts for making 3d crevasse domains
    refined_box_gmshapi_extruded_crevassefirst.py - makes domain
    with crevasse oriented at 90deg.
    
    refined_box_gmshapi_extruded_crevassefirst_rotated.py rotates the
    crevasse to be at agle of 60 deg (or 30deg) to the side walls.

--- run_scripts_testing/ice_shelf_cavity/refined_box_gmshapi_extruded_crevassefirst_rotated.py
+++ run_scripts_testing/ice_shelf_cavity/refined_box_gmshapi_extruded_crevassefirst_rotated.py
@@ -10,17 +10,29 @@ gmsh.model.add("2d_crevasse_flume")
 lc = 250
 L = 5000
 #add crevasse
-gmsh.model.geo.addPoint(2400, 0.0, 0, 5, 21)
-gmsh.model.geo.addPoint(2475, 0.0, 50, 5, 22)
-gmsh.model.geo.addPoint(2525, 0.0, 50, 5, 23)
-gmsh.model.geo.addPoint(2600, 0.0, 0, 5, 24)
+gmsh.model.geo.addPoint(3800, 0.0, 0, 5, 21)
+gmsh.model.geo.addPoint(3875, 0.0, 50, 5, 22)
+gmsh.model.geo.addPoint(3925, 0.0, 50, 5, 23)
+gmsh.model.geo.addPoint(4000, 0.0, 0, 5, 24)
 
 
-gmsh.model.geo.addPoint(2400, L, 0, 5, 25)
-gmsh.model.geo.addPoint(2475, L, 50, 5, 26)
-gmsh.model.geo.addPoint(2525, L, 50, 5, 27)
-gmsh.model.geo.addPoint(2600, L, 0, 5, 28)
+gmsh.model.geo.addPoint(1000, L, 0, 5, 25)
+gmsh.model.geo.addPoint(1075, L, 50, 5, 26)
+gmsh.model.geo.addPoint(1125, L, 50, 5, 27)
+gmsh.model.geo.addPoint(1200, L, 0, 5, 28)
+'''
+# positive slant of 59 deg. i.e tan (5km/3km) = 59deg
+gmsh.model.geo.addPoint(1000, 0.0, 0, 5, 21)
+gmsh.model.geo.addPoint(1075, 0.0, 50, 5, 22)
+gmsh.model.geo.addPoint(1125, 0.0, 50, 5, 23)
+gmsh.model.geo.addPoint(1200, 0.0, 0, 5, 24)
 
+
+gmsh.model.geo.addPoint(3800, L, 0, 5, 25)
+gmsh.model.geo.addPoint(3875, L, 50, 5, 26)
+gmsh.model.geo.addPoint(3925, L, 50, 5, 27)
+gmsh.model.geo.addPoint(4000, L, 0, 5, 28)
+'''
 
 
 
@@ -97,36 +109,58 @@ gmsh.model.geo.addPlaneSurface([2], 2)
 # (limited by 1 node singularity solution...) 
 # try 20m horizontal but keep 5m vertical.
 # Let's use the minimum of all the fields as the background mesh field:
-gmsh.model.mesh.field.add("Box", 1)
-gmsh.model.mesh.field.setNumber(1, "VIn", lc / 12.5)
-gmsh.model.mesh.field.setNumber(1, "VOut", lc)
-gmsh.model.mesh.field.setNumber(1, "XMin", 2400)
-gmsh.model.mesh.field.setNumber(1, "XMax", 2600)
-gmsh.model.mesh.field.setNumber(1, "YMin", 0)
-gmsh.model.mesh.field.setNumber(1, "YMax", 5000)
-gmsh.model.mesh.field.setNumber(1, "Thickness", 1000)
+#below box is for 90deg alignment!!!
+#gmsh.model.mesh.field.add("Box", 1)
+#gmsh.model.mesh.field.setNumber(1, "VIn", lc / 12.5)
+#gmsh.model.mesh.field.setNumber(1, "VOut", lc)
+#gmsh.model.mesh.field.setNumber(1, "XMin", 2400)
+#gmsh.model.mesh.field.setNumber(1, "XMax", 2600)
+#gmsh.model.mesh.field.setNumber(1, "YMin", 0)
+#gmsh.model.mesh.field.setNumber(1, "YMax", 5000)
+#gmsh.model.mesh.field.setNumber(1, "Thickness", 1000)
 
-gmsh.model.mesh.field.add("Box", 2)
-gmsh.model.mesh.field.setNumber(2, "VIn", 5)
-gmsh.model.mesh.field.setNumber(2, "VOut", lc)
-gmsh.model.mesh.field.setNumber(2, "XMin", 0)
-gmsh.model.mesh.field.setNumber(2, "XMax", 5000)
-gmsh.model.mesh.field.setNumber(2, "YMin", 0)
-gmsh.model.mesh.field.setNumber(2, "YMax", 5000)
-gmsh.model.mesh.field.setNumber(2, "ZMin", 5)
-gmsh.model.mesh.field.setNumber(2, "ZMax", 50)
-gmsh.model.mesh.field.setNumber(2, "Thickness", 10)
-gmsh.model.mesh.field.add("Min", 3)
-gmsh.model.mesh.field.setNumbers(3, "FieldsList", [1,2])
 
-gmsh.model.mesh.field.setAsBackgroundMesh(3)
+gmsh.model.geo.synchronize()
+#  higher res inside crevasse - use lines 29 and 30 
+
+gmsh.model.mesh.field.add("Distance", 1)
+gmsh.model.mesh.field.setNumbers(1, "CurvesList", [29, 30])
+gmsh.model.mesh.field.setNumber(1, "Sampling", 1000)
+#gmsh.model.mesh.field.add("Threshold", 2)
+#gmsh.model.mesh.field.setNumber(2, "DistMax", 1000)
+#gmsh.model.mesh.field.setNumber(2, "DistMin", 100)
+#gmsh.model.mesh.field.setNumber(2, "InField", 1)
+#gmsh.model.mesh.field.setNumber(2, "SizeMin", lc / 12.5)
+#gmsh.model.mesh.field.setNumber(2, "SizeMax", lc)
+gmsh.model.mesh.field.add("Threshold", 2)
+gmsh.model.mesh.field.setNumber(2, "InField", 1)
+gmsh.model.mesh.field.setNumber(2, "SizeMin", lc / 12.5)
+gmsh.model.mesh.field.setNumber(2, "SizeMax", lc)
+gmsh.model.mesh.field.setNumber(2, "DistMin", 100)
+gmsh.model.mesh.field.setNumber(2, "DistMax", 500)
+
+
+
+gmsh.model.mesh.field.add("Box", 3)
+gmsh.model.mesh.field.setNumber(3, "VIn", 5)
+gmsh.model.mesh.field.setNumber(3, "VOut", lc)
+gmsh.model.mesh.field.setNumber(3, "XMin", 0)
+gmsh.model.mesh.field.setNumber(3, "XMax", 5000)
+gmsh.model.mesh.field.setNumber(3, "YMin", 0)
+gmsh.model.mesh.field.setNumber(3, "YMax", 5000)
+gmsh.model.mesh.field.setNumber(3, "ZMin", 5)
+gmsh.model.mesh.field.setNumber(3, "ZMax", 50)
+gmsh.model.mesh.field.setNumber(3, "Thickness", 10)
+gmsh.model.mesh.field.add("Min", 4)
+gmsh.model.mesh.field.setNumbers(4, "FieldsList", [2,3])
+
+gmsh.model.mesh.field.setAsBackgroundMesh(4)
 gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
 gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
 gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
 gmsh.option.setNumber("Mesh.Algorithm", 5)
 
 gmsh.model.mesh.generate(2)
-
 
 h = -100
 ov = gmsh.model.geo.extrude([(2, 1), (2,56), (2,2)], 0, 0, h, [20])
@@ -148,5 +182,5 @@ gmsh.model.addPhysicalGroup(3, [ov[1][1],ov[7][1],ov[13][1]], 101)
 gmsh.model.geo.synchronize()
 #gmsh.write("3d_crevasse_flume_test_extrapoints.geo_unrolled")
 gmsh.model.mesh.generate(3)
-gmsh.write("3d_crevasse_flume_dx250mto20m_dz5m_crevdxz5m.msh")
+gmsh.write("3d_crevasse_flume_dx250mto20m_dz5m_crevdxz5m_rotated_60degneg.msh")
 gmsh.finalize()
